51f3ca006c4934ea615c85fe6ce3509a582c8054
carolin santoro <carolin_santoro@yahoo.com.br>
2019-08-20 16:47:01 -0300

    um monte de coisas realizadas

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+# see all tasks
+get "/tasks",         to: "tasks#index"
+# form for create a task
+get "tasks/new",      to: "tasks#new"
+# create a task
+post "tasks",         to: "tasks#create"
+# see one task
+get "/tasks/:id",     to: "tasks#show", as: "task"
+# form for update a task
+get "tasks/:id/edit", to: "tasks#edit"
+# update a task
+patch "tasks/:id",    to: "tasks#update"
+# delete a tasks
+delete "tasks/:id",  to: "tasks#destroy"
 end
